9ba4c9d3e78dbd19105e2cff482a6a76b3e60a17
jacksweat <jacksweat93@outlook.com>
2018-09-29 23:06:42 +0800

    crazy.blade fix

--- resources/views/frontend-templates/single-product/crazy/crazy.blade.php
+++ resources/views/frontend-templates/single-product/crazy/crazy.blade.php
@@ -459,7 +459,7 @@
                 </div> 
 
                 <div class="single-product-bottom-section">
-                  <h3>{!! get_product_title($products['id']) !!}</h3>
+                  <h3>{!! get_product_title($products['id']) !!} </h3>
 
                   @if(get_product_type($products['id']) == 'simple_product')
                     <p>{!! price_html( get_product_price($products['id']), get_frontend_selected_currency() ) !!}</p>
